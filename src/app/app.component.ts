edb3ca1c6a0aa0b518d58c952e868d7f35766eed
Indra Tanaya <imadearyaindra.tanaya@csgi.com>
2022-11-07 11:01:26 +0800

    Create a custom element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Injector } from '@angular/core';
+import {
+  createCustomElement,
+  NgElement,
+  WithProperties,
+} from '@angular/elements';
+import { NamePanelComponent } from './name-panel/name-panel.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(injector: Injector) {
+    // Convert `NamePanelComponent` to a custom element
+    const namePanelElement = createCustomElement(NamePanelComponent, {
+      injector,
+    });
+    // Register the custom element with the browser
+    customElements.define('el-name-panel', namePanelElement);
+  }
+
+  showNamePanel() {
+    // Create element
+    const namePanelElement: NgElement & WithProperties<NamePanelComponent> =
+      document.createElement('el-name-panel') as any;
+    // Add to the DOM
+    document.body.appendChild(namePanelElement);
+  }
+}
